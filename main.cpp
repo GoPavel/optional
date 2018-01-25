07feb5227384504eb5baf64fcec640442c7fc372
Golovin Pavel <gopavel0@gmail.com>
2018-01-25 11:19:46 +0300

    first  commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "optional.h"
+
+template class optional<int>;
+
+int main () {
+    std::cout << "OPTIONAL" << std::endl;
+
+
+
+    return 0;
+}
---
+++ optional.h
@@ -0,0 +1,83 @@
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
+#include <cassert>
+
+template <typename T>
+class optional{
+    bool is_exist;
+    char storage[sizeof(T)];
+
+public:
+    optional(): is_exist(false) { }
+
+    optional(optional const &other): is_exist(other.is_exist) {
+        if (is_exist) {
+            new (get_ptr()) T(*other.get_ptr());
+        }
+    }
+
+    optional(optional &&other): is_exist(other.is_exist) {
+        if (is_exist) {
+            new (get_ptr()) T(std::move(*other.get_ptr()));
+        }
+    }
+
+    optional(T const &obj): is_exist(true) {
+        new (get_ptr()) T(obj);
+    }
+
+    optional(T &&obj): is_exist(true) {
+        new (get_ptr()) T(obj);
+    }
+
+    optional& operator=(optional const &other) {
+        if (is_exist) {
+            (*get_ptr()).T::~T();
+        }
+        is_exist = other.is_exist;
+        if (is_exist) {
+            new (get_ptr()) T(*other.get_ptr());
+        }
+        return *this;
+    }
+
+    optional& operator=(optional &&other) {
+        if (is_exist) {
+            (*get_ptr()).T::~T();
+        }
+        is_exist = other.is_exist;
+        if(is_exist) {
+            new (get_ptr()) T(std::move(*other.get_ptr()));
+        }
+        return *this;
+    }
+
+    T& operator*() {
+        assert(is_exist);
+        return *get_ptr();
+    }
+
+    T* operator->() {
+        assert(is_exist);
+        return get_ptr();
+    }
+
+    bool is_empty() {
+        return is_exist;
+    }
+
+    operator bool() {
+        return is_exist;
+    }
+private:
+
+    T* get_ptr() {
+        return reinterpret_cast<T *&>(storage);
+    }
+
+    T const* get_ptr() const {
+        return reinterpret_cast<T const * const &>(storage);
+    }
+};
+
+#endif //OPTIONAL_H
